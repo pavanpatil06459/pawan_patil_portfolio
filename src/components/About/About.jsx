30a76cc0a30ba5ab4ad738fcacb06a7ba232cfad
pavanpatil06459 <pavanpatil06459@gmail.com>
2026-05-13 19:50:15 +0530

    spell mistake

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -25,7 +25,7 @@ const About = () => {
             <span className="text-white">I am a </span>
             <ReactTypingEffect
               text={[
-                'Fronted Developer',
+                'Frontend Developer',
                 'Coder',
                 
                 
